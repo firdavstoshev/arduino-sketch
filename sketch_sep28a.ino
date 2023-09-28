6f5241e0164b8b37e01d129052ffad101a9d4d8d
Firdavs Toshev <iamfirdavs2002@gmail.com>
2023-09-28 21:51:39 +0500

    init commit

---
+++ sketch_sep28a.ino
@@ -0,0 +1,19 @@
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+
+    char command = Serial.read();
+
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
